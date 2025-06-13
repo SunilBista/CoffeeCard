4f8364a67ff475a98d6e82990e89224ee29aab19
SunilBista <sunilbista303@gmail.com>
2025-06-13 12:08:00 +1000

    feat: learn column feature in sandbox column

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,26 @@ import 'package:flutter/material.dart'; //to add MaterialAPP widget
 void main() {
   runApp(MaterialApp(home: Home()));
 }
+
+class SandboxColumn extends StatelessWidget {
+  const SandboxColumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(width: 100, color: Colors.red, child: Text('one')),
+          Container(width: 200, color: Colors.green, child: Text('two')),
+          Container(width: 300, color: Colors.blue, child: Text('three')),
+        ],
+      ),
+    );
+  }
+}
