1ebd578ff585964e100c0f0dbf7eaee2bfcd7f44
SunilBista <sunilbista303@gmail.com>
2025-06-13 12:08:18 +1000

    feat: add column in home class

--- lib/home.dart
+++ lib/home.dart
@@ -7,11 +7,28 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Coffee Title'),
+        title: const Text(
+          'My Coffee Title',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
         backgroundColor: Colors.brown[700],
         centerTitle: true,
       ),
-      body: Text('Hello'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.brown[200],
+            padding: EdgeInsets.all(20),
+            child: Text('How I like my coffee...'),
+          ),
+          Container(
+            color: Colors.brown[100],
+            padding: EdgeInsets.all(20),
+            child: Text('Coffee Prefs..'),
+          ),
+        ],
+      ),
     );
   }
 }
