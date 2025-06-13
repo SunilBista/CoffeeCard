4f8fefb87e84d5a4974efef9220a148b8bca9a14
SunilBista <sunilbista303@gmail.com>
2025-06-13 11:52:57 +1000

    import Home to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,6 @@
+import 'package:coffee_card/home.dart';
 import 'package:flutter/material.dart'; //to add MaterialAPP widget
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My Coffee Title'),
-          backgroundColor: Colors.brown[700],
-          centerTitle: true,
-        ),
-        body: const Home(),
-      ),
-    ),
-  );
+  runApp(MaterialApp(home: Home()));
 }
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override //override inherited
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange,
-      // width: 200,
-      // height: 100,
-      padding: EdgeInsets.all(20),
-      margin: EdgeInsets.fromLTRB(10, 40, 0, 0),
-      child: Text(
-        'Hello World',
-        style: TextStyle(
-          fontSize: 18,
-          letterSpacing: 4,
-          decoration: TextDecoration.underline,
-          fontStyle: FontStyle.italic,
-        ),
-      ),
-    );
-  }
-}
-
-//container
